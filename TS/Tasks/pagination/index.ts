c8212c9e88898efd759ee8b7026d8ed7f8d64117
킨 <69646118+Roflane@users.noreply.github.com>
2025-11-24 19:53:30 +0400

    Update index.ts

--- TS/Tasks/pagination/index.ts
+++ TS/Tasks/pagination/index.ts
@@ -1,6 +1,6 @@
 let currentPage = 1;
 let totalPages = 1;
-let secret: string = '';
+let secret: string = "9a447a4d";
 
 function createPopupWindow(item: any) {
     if (document.querySelector(".window-overlay")) return;
